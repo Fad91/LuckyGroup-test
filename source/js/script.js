39f75a097c521348a3d9b0df57d214460baddef7
Антон Куклин <homyak91@inbox.ru>
2021-06-16 10:28:53 +0400

    upd

--- source/js/script.js
+++ source/js/script.js
@@ -14,8 +14,11 @@ for (const option of document.querySelectorAll(".custom-option")) {
       if (!this.classList.contains('selected')) {
         // если у него нет класса селектед
           this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+          // у родительского элемента, который был выбран до нынешнего элемента убирается класс
           this.classList.add('selected');
+          // новому добавляется
           this.closest('.custom-select').querySelector('.custom-select__trigger p').innerHTML = this.innerHTML;
+          // задает селекту новое значение, равное выбранному элементу
       }
   })
 }
